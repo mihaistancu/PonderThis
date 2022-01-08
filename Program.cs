f78bc61b1c54d5f365b9f3f1262301def3ebb862
mihai stancu <Mihai-Dorian.STANCU@ext.ec.europa.eu>
2022-01-08 09:04:25 +0100

    Generate 5 digit primes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var primes = Get5DigitPrimes();
+
+List<int> Get5DigitPrimes()
+{
+    List<int> result = new List<int>();
+
+    for (int i=10000; i<99999; i++) 
+    {
+        if (IsPrime(i)) 
+        {
+            result.Add(i);
+        }
+    }
+
+    return result;
+}
+
+bool IsPrime(int n)
+{
+    double limit = Math.Sqrt(n);
+
+    for (int i=2;i<limit;i++) {
+        if (n % i == 0) return false;
+    }    
+
+    return true;
+}
