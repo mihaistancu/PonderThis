0946033753eb52e7a07b66ac2e8d762a0024c9fe
mihai stancu <Mihai-Dorian.STANCU@ext.ec.europa.eu>
2022-01-08 10:04:01 +0100

    Get primes with 5 distinct digits

--- Program.cs
+++ Program.cs
@@ -1,18 +1,35 @@
-﻿var primes = Get5DigitPrimes();
+﻿
+var primes = GetPrimesWith5DistinctDigits();
 
-List<int> Get5DigitPrimes()
+Console.WriteLine(primes.Count);
+
+List<int> GetPrimesWith5DistinctDigits()
 {
     List<int> result = new List<int>();
 
     for (int i=10000; i<99999; i++) 
     {
-        if (IsPrime(i)) 
+        if (IsPrime(i) && AreDigitsDistinct(i)) 
         {
             result.Add(i);
         }
     }
 
     return result;
+}
+
+bool AreDigitsDistinct(int n) 
+{
+    int[] digits = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+    while (n / 10 > 0) 
+    {
+        n = n / 10;
+        if (digits[n % 10] > 0) return false;
+        digits[n % 10]++;
+    }
+
+    return true;
 }
 
 bool IsPrime(int n)
